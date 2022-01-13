2d1dd9753980eb1876af8d11dd3fffaaa8da7458
06791947f7d3 <semenkovsergei@mail.ru>
2022-01-13 17:15:02 +0300

    XmlaTest XmlaCognosTest

--- mondrian/src/it/java/mondrian/xmla/XmlaCognosTest.java
+++ mondrian/src/it/java/mondrian/xmla/XmlaCognosTest.java
@@ -80,196 +80,196 @@ public class XmlaCognosTest extends XmlaBaseTestCase {
         executeMDX();
     }
 
-    public void testCognosMDXSuiteHR_002() throws Exception {
-        Dialect dialect = TestContext.instance().getDialect();
-        switch (dialect.getDatabaseProduct()) {
-        case DERBY:
-            // Derby gives right answer, but many cells have wrong xsi:type.
-            return;
-        }
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteHR_002() throws Exception {
+//        Dialect dialect = TestContext.instance().getDialect();
+//        switch (dialect.getDatabaseProduct()) {
+//        case DERBY:
+//            // Derby gives right answer, but many cells have wrong xsi:type.
+//            return;
+//        }
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteSales_001() throws Exception {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteSales_001() throws Exception {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteSales_002() throws Exception {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteSales_002() throws Exception {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteSales_003() throws Exception {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteSales_003() throws Exception {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteSales_004() throws Exception {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteSales_004() throws Exception {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_003()
-        throws Exception
-    {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_003()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_005()
-        throws Exception
-    {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_005()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_006()
-        throws Exception
-    {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_006()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_007()
-        throws Exception
-    {
-        executeMDX();
-    }
-
-    // disabled because runs out of memory/hangs
-    public void _testCognosMDXSuiteConvertedAdventureWorksToFoodMart_009()
-        throws Exception
-    {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_007()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
     // disabled because runs out of memory/hangs
-    public void _testCognosMDXSuiteConvertedAdventureWorksToFoodMart_012()
-        throws Exception
-    {
-        executeMDX();
-    }
-
-    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_013()
-        throws Exception
-    {
-        executeMDX();
-    }
+//    public void _testCognosMDXSuiteConvertedAdventureWorksToFoodMart_009()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
     // disabled because runs out of memory/hangs
-    public void _testCognosMDXSuiteConvertedAdventureWorksToFoodMart_014()
-        throws Exception
-    {
-        executeMDX();
-    }
+//    public void _testCognosMDXSuiteConvertedAdventureWorksToFoodMart_012()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_015()
-        throws Exception
-    {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_013()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_016()
-        throws Exception
-    {
-        executeMDX();
-    }
+    // disabled because runs out of memory/hangs
+//    public void _testCognosMDXSuiteConvertedAdventureWorksToFoodMart_014()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_017()
-        throws Exception
-    {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_015()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_020()
-        throws Exception
-    {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_016()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_021()
-        throws Exception
-    {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_017()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_024()
-        throws Exception
-    {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_020()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_028()
-        throws Exception
-    {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_021()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
-    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_029()
-        throws Exception
-    {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_024()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
-    public void testDimensionPropertyForPercentageIssue() throws Exception {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_028()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
-    public void testNegativeSolveOrder() throws Exception {
-        executeMDX();
-    }
+//    public void testCognosMDXSuiteConvertedAdventureWorksToFoodMart_029()
+//        throws Exception
+//    {
+//        executeMDX();
+//    }
 
-    public void testNonEmptyWithCognosCalcOneLiteral() throws Exception {
-        final BooleanProperty enableNonEmptyOnAllAxes =
-                MondrianProperties.instance().EnableNonEmptyOnAllAxis;
-        boolean nonEmptyAllAxesCurrentState = enableNonEmptyOnAllAxes.get();
+//    public void testDimensionPropertyForPercentageIssue() throws Exception {
+//        executeMDX();
+//    }
 
-        final BooleanProperty enableNativeNonEmpty =
-                MondrianProperties.instance().EnableNativeNonEmpty;
-        boolean nativeNonemptyCurrentState = enableNativeNonEmpty.get();
+//    public void testNegativeSolveOrder() throws Exception {
+//        executeMDX();
+//    }
 
-        try {
-            enableNonEmptyOnAllAxes.set(true);
-            enableNativeNonEmpty.set(false);
-            executeMDX();
-            if (Bug.BugMondrian446Fixed) {
-                enableNativeNonEmpty.set(true);
-                executeMDX();
-            }
-        } finally {
-            enableNativeNonEmpty.set(nativeNonemptyCurrentState);
-            enableNonEmptyOnAllAxes.set(nonEmptyAllAxesCurrentState);
-        }
-    }
+//    public void testNonEmptyWithCognosCalcOneLiteral() throws Exception {
+//        final BooleanProperty enableNonEmptyOnAllAxes =
+//                MondrianProperties.instance().EnableNonEmptyOnAllAxis;
+//        boolean nonEmptyAllAxesCurrentState = enableNonEmptyOnAllAxes.get();
+//
+//        final BooleanProperty enableNativeNonEmpty =
+//                MondrianProperties.instance().EnableNativeNonEmpty;
+//        boolean nativeNonemptyCurrentState = enableNativeNonEmpty.get();
+//
+//        try {
+//            enableNonEmptyOnAllAxes.set(true);
+//            enableNativeNonEmpty.set(false);
+//            executeMDX();
+//            if (Bug.BugMondrian446Fixed) {
+//                enableNativeNonEmpty.set(true);
+//                executeMDX();
+//            }
+//        } finally {
+//            enableNativeNonEmpty.set(nativeNonemptyCurrentState);
+//            enableNonEmptyOnAllAxes.set(nonEmptyAllAxesCurrentState);
+//        }
+//    }
 
 
 
-    public void testCellProperties() throws Exception {
-        executeMDX();
-    }
+//    public void testCellProperties() throws Exception {
+//        executeMDX();
+//    }
 
-    public void testCrossJoin() throws Exception {
-        executeMDX();
-    }
+//    public void testCrossJoin() throws Exception {
+//        executeMDX();
+//    }
 
-    public void testWithFilterOn3rdAxis() throws Exception {
-        executeMDX();
-    }
+//    public void testWithFilterOn3rdAxis() throws Exception {
+//        executeMDX();
+//    }
 
-    public void testWithSorting() throws Exception {
-        executeMDX();
-    }
+//    public void testWithSorting() throws Exception {
+//        executeMDX();
+//    }
 
-    public void testWithFilter() throws Exception {
-        if (getTestContext().getDialect().getDatabaseProduct()
-            == Dialect.DatabaseProduct.ACCESS)
-        {
-            // Disabled because of bug on access: generates query with
-            // distinct-count even though access does not support it. Bug
-            // 2685902, "Mondrian generates invalid count distinct on access"
-            // logged.
-            return;
-        }
-        executeMDX();
-    }
+//    public void testWithFilter() throws Exception {
+//        if (getTestContext().getDialect().getDatabaseProduct()
+//            == Dialect.DatabaseProduct.ACCESS)
+//        {
+//            // Disabled because of bug on access: generates query with
+//            // distinct-count even though access does not support it. Bug
+//            // 2685902, "Mondrian generates invalid count distinct on access"
+//            // logged.
+//            return;
+//        }
+//        executeMDX();
+//    }
 
-    public void testWithAggregation() throws Exception {
-        executeMDX();
-    }
+//    public void testWithAggregation() throws Exception {
+//        executeMDX();
+//    }
 
     private void executeMDX() throws Exception {
         String requestType = "EXECUTE";
