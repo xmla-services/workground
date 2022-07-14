480a86141bad6e4be835d762b4af5a9c7895c2b9
dbulahov <bulahovdenis@gmail.com>
2022-07-14 10:04:51 +0300

    migrate SegmentCacheTest

---
+++ org.opencube.emondrian.tests/src/main/java/mondrian/rolap/agg/SegmentCacheTest.java
@@ -0,0 +1,163 @@
+/*
+* This software is subject to the terms of the Eclipse Public License v1.0
+* Agreement, available at the following URL:
+* http://www.eclipse.org/legal/epl-v10.html.
+* You must accept the terms of that agreement to use this software.
+*
+* Copyright (c) 2002-2017 Hitachi Vantara..  All rights reserved.
+*/
+
+package mondrian.rolap.agg;
+
+import mondrian.olap.*;
+import mondrian.rolap.agg.SegmentCacheManager.CompositeSegmentCache;
+import mondrian.spi.SegmentCache;
+import mondrian.spi.SegmentHeader;
+import mondrian.test.BasicQueryTest;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.opencube.junit5.ContextSource;
+import org.opencube.junit5.context.Context;
+import org.opencube.junit5.dataloader.FastFoodmardDataLoader;
+import org.opencube.junit5.propupdator.AppandFoodMartCatalogAsFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.opencube.junit5.TestUtil.assertQueryReturns;
+import static org.opencube.junit5.TestUtil.executeQuery;
+
+/**
+ * Test suite that runs the {@link BasicQueryTest} but with the
+ * {@link MockSegmentCache} active.
+ *
+ * @author LBoudreau
+ */
+public class SegmentCacheTest {
+
+	@ParameterizedTest
+	@ContextSource(propertyUpdater = AppandFoodMartCatalogAsFile.class, dataloader = FastFoodmardDataLoader.class)
+    public void testCompoundPredicatesCollision(Context context) {
+        String query =
+            "SELECT [Gender].[All Gender] ON 0, [MEASURES].[CUSTOMER COUNT] ON 1 FROM SALES";
+        String query2 =
+            "WITH MEMBER GENDER.X AS 'AGGREGATE({[GENDER].[GENDER].members} * "
+            + "{[STORE].[ALL STORES].[USA].[CA]})', solve_order=100 "
+            + "SELECT GENDER.X ON 0, [MEASURES].[CUSTOMER COUNT] ON 1 FROM SALES";
+        String result =
+            "Axis #0:\n"
+            + "{}\n"
+            + "Axis #1:\n"
+            + "{[Gender].[All Gender]}\n"
+            + "Axis #2:\n"
+            + "{[Measures].[Customer Count]}\n"
+            + "Row #0: 5,581\n";
+        String result2 =
+            "Axis #0:\n"
+            + "{}\n"
+            + "Axis #1:\n"
+            + "{[Gender].[X]}\n"
+            + "Axis #2:\n"
+            + "{[Measures].[Customer Count]}\n"
+            + "Row #0: 2,716\n";
+        Connection connection = context.createConnection();
+        connection.getCacheControl(null).flushSchemaCache();
+        assertQueryReturns(connection, query, result);
+        assertQueryReturns(connection, query2, result2);
+    }
+
+	@ParameterizedTest
+	@ContextSource(propertyUpdater = AppandFoodMartCatalogAsFile.class, dataloader = FastFoodmardDataLoader.class)	
+    public void testSegmentCacheEvents(Context context) throws Exception {
+        SegmentCache mockCache = new MockSegmentCache();
+        SegmentCacheWorker testWorker =
+            new SegmentCacheWorker(mockCache, null);
+
+        // Flush the cache before we start. Wait a second for the cache
+        // flush to propagate.
+        Connection connection = context.createConnection();
+        connection.getCacheControl(null).flushSchemaCache();
+        final CacheControl cc =
+                connection.getCacheControl(null);
+        Cube salesCube = getCube(connection, "Sales");
+        cc.flush(cc.createMeasuresRegion(salesCube));
+        Thread.sleep(1000);
+
+        MondrianServer.forConnection(connection)
+            .getAggregationManager().cacheMgr.segmentCacheWorkers
+            .add(testWorker);
+
+        final List<SegmentHeader> createdHeaders =
+            new ArrayList<SegmentHeader>();
+        final List<SegmentHeader> deletedHeaders =
+            new ArrayList<SegmentHeader>();
+        final SegmentCache.SegmentCacheListener listener =
+            new SegmentCache.SegmentCacheListener() {
+                public void handle(SegmentCacheEvent e) {
+                    switch (e.getEventType()) {
+                    case ENTRY_CREATED:
+                        createdHeaders.add(e.getSource());
+                        break;
+                    case ENTRY_DELETED:
+                        deletedHeaders.add(e.getSource());
+                        break;
+                    default:
+                        throw new UnsupportedOperationException();
+                    }
+                }
+            };
+
+        try {
+            // Register our custom listener.
+            ((CompositeSegmentCache)MondrianServer
+                .forConnection(connection)
+                .getAggregationManager().cacheMgr.compositeCache)
+                .addListener(listener);
+            // Now execute a query and check the events
+            executeQuery(connection,
+                "select {[Measures].[Unit Sales]} on columns from [Sales]");
+            // Wait for propagation.
+            Thread.sleep(2000);
+            assertEquals(2, createdHeaders.size());
+            assertEquals(0, deletedHeaders.size());
+            assertEquals("Sales", createdHeaders.get(0).cubeName);
+            assertEquals("FoodMart", createdHeaders.get(0).schemaName);
+            assertEquals("Unit Sales", createdHeaders.get(0).measureName);
+            createdHeaders.clear();
+            deletedHeaders.clear();
+
+            // Now flush the segment and check the events.
+            cc.flush(cc.createMeasuresRegion(salesCube));
+
+            // Wait for propagation.
+            Thread.sleep(2000);
+            assertEquals(0, createdHeaders.size());
+            assertEquals(2, deletedHeaders.size());
+            assertEquals("Sales", deletedHeaders.get(0).cubeName);
+            assertEquals("FoodMart", deletedHeaders.get(0).schemaName);
+            assertEquals("Unit Sales", deletedHeaders.get(0).measureName);
+        } finally {
+            ((CompositeSegmentCache)MondrianServer
+                .forConnection(connection)
+                .getAggregationManager().cacheMgr.compositeCache)
+                .removeListener(listener);
+            MondrianServer.forConnection(connection)
+                .getAggregationManager().cacheMgr.segmentCacheWorkers
+                .remove(testWorker);
+        }
+    }
+
+    private Cube getCube(Connection connection, String cubeName) {
+        for (Cube cube
+            : connection.getSchemaReader().withLocus().getCubes())
+        {
+            if (cube.getName().equals(cubeName)) {
+                return cube;
+            }
+        }
+        return null;
+    }
+}
+
+// End SegmentCacheTest.java
