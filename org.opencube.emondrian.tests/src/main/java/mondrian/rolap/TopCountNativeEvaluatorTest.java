f153dfaff9ddae3dd5726c54551b95471f1eea8f
dbulahov <bulahovdenis@gmail.com>
2022-07-20 17:52:14 +0300

    migrate SqlConstraintUtilsTest, TopCountNativeEvaluatorTest, TopCountWithTwoParamsVersusHeadTest

---
+++ org.opencube.emondrian.tests/src/main/java/mondrian/rolap/TopCountNativeEvaluatorTest.java
@@ -0,0 +1,109 @@
+/*
+// This software is subject to the terms of the Eclipse Public License v1.0
+// Agreement, available at the following URL:
+// http://www.eclipse.org/legal/epl-v10.html.
+// You must accept the terms of that agreement to use this software.
+//
+// Copyright (C) 2015-2017 Hitachi Vantara and others
+// All Rights Reserved.
+*/
+package mondrian.rolap;
+
+import mondrian.calc.DummyExp;
+import mondrian.olap.Exp;
+import mondrian.olap.FunDef;
+import mondrian.olap.Literal;
+import mondrian.olap.type.EmptyType;
+import mondrian.test.PropertySaver5;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.*;
+
+/**
+ * This class contains tests for some cases related to creating
+ * native evaluator for {@code TOPCOUNT} function.
+ *
+ * @author Andrey Khayrutdinov
+ * @see RolapNativeTopCount#createEvaluator(RolapEvaluator, FunDef, Exp[])
+ */
+public class TopCountNativeEvaluatorTest {
+
+    private PropertySaver5 propSaver;
+    @BeforeEach
+    public void beforeEach() {
+        propSaver = new PropertySaver5();
+        propSaver.set(propSaver.properties.EnableNativeTopCount, true);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        propSaver.reset();
+    }
+
+    @Test
+    public void testNonNative_WhenExplicitlyDisabled() throws Exception {
+        propSaver.set(propSaver.properties.EnableNativeTopCount, false);
+        RolapNativeTopCount nativeTopCount = new RolapNativeTopCount();
+
+        assertNull(
+                nativeTopCount.createEvaluator(null, null, null),
+                "Native evaluator should not be created when "
+                        + "'mondrian.native.topcount.enable' is 'false'");
+    }
+
+    @Test
+    public void testNonNative_WhenContextIsInvalid() throws Exception {
+        RolapNativeTopCount nativeTopCount = createTopCountSpy();
+        doReturn(false).when(nativeTopCount)
+            .isValidContext(any(RolapEvaluator.class));
+
+        assertNull(
+            nativeTopCount.createEvaluator(null, null, null), "Native evaluator should not be created when "
+                        + "evaluation context is invalid");
+    }
+
+    /**
+     * For now, prohibit native evaluation of the function if has two
+     * parameters. According to the specification, this means
+     * the function should behave similarly to {@code HEAD} function.
+     * However, native evaluation joins data with the fact table and if there
+     * is no data there, then some records are ignored, what is not correct.
+     *
+     * @see <a href="http://jira.pentaho.com/browse/MONDRIAN-2394">MONDRIAN-2394</a>
+     */
+    @Test
+    public void testNonNative_WhenTwoParametersArePassed() throws Exception {
+        RolapNativeTopCount nativeTopCount = createTopCountSpy();
+        doReturn(true).when(nativeTopCount)
+            .isValidContext(any(RolapEvaluator.class));
+
+        Exp[] arguments = new Exp[] {
+            new DummyExp(new EmptyType()),
+            Literal.create(BigDecimal.ONE)
+        };
+
+        assertNull(
+            nativeTopCount.createEvaluator(
+                null, mockFunctionDef(), arguments),  "Native evaluator should not be created when "
+                        + "two parameters are passed");
+    }
+
+    private RolapNativeTopCount createTopCountSpy() {
+        RolapNativeTopCount nativeTopCount = new RolapNativeTopCount();
+        nativeTopCount = spy(nativeTopCount);
+        return nativeTopCount;
+    }
+
+    private FunDef mockFunctionDef() {
+        FunDef topCountFunMock = mock(FunDef.class);
+        when(topCountFunMock.getName()).thenReturn("TOPCOUNT");
+        return topCountFunMock;
+    }
+}
+
+// End TopCountNativeEvaluatorTest.java
---
+++ org.opencube.emondrian.tests/src/main/java/mondrian/rolap/TopCountWithTwoParamsVersusHeadTest.java
@@ -0,0 +1,102 @@
+/*
+// This software is subject to the terms of the Eclipse Public License v1.0
+// Agreement, available at the following URL:
+// http://www.eclipse.org/legal/epl-v10.html.
+// You must accept the terms of that agreement to use this software.
+//
+// Copyright (C) 2015-2017 Hitachi Vantara and others
+// All Rights Reserved.
+*/
+package mondrian.rolap;
+
+import mondrian.olap.Connection;
+import mondrian.olap.Result;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.opencube.junit5.ContextSource;
+import org.opencube.junit5.TestUtil;
+import org.opencube.junit5.context.Context;
+import org.opencube.junit5.dataloader.FastFoodmardDataLoader;
+import org.opencube.junit5.propupdator.AppandFoodMartCatalogAsFile;
+
+import static mondrian.rolap.RolapNativeTopCountTestCases.NON_EMPTY_IS_NOT_IGNORED_WHEN_TWO_PARAMS_QUERY;
+import static mondrian.rolap.RolapNativeTopCountTestCases.RESULTS_ARE_SHOWN_NOT_MORE_THAN_EXIST_2_PARAMS_QUERY;
+import static mondrian.rolap.RolapNativeTopCountTestCases.TOPCOUNT_MIMICS_HEAD_WHEN_TWO_PARAMS_CITIES_QUERY;
+import static mondrian.rolap.RolapNativeTopCountTestCases.TOPCOUNT_MIMICS_HEAD_WHEN_TWO_PARAMS_STATES_QUERY;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.opencube.junit5.TestUtil.flushSchemaCache;
+
+/**
+ * According to
+ * <a href="https://msdn.microsoft.com/en-us/library/ms144792.aspx">MSDN
+ * page</a>, when {@code TOPCOUNT} function is called with two parameters
+ * it should mimic the behaviour of {@code HEAD} function.
+ * <p/>
+ * The idea of these tests is to compare results of both function being
+ * called with same parameters - they should be equal.
+ * <p/>
+ * Queries with {@code HEAD} are made by mere substitution of the name
+ * instead of {@code TOPCOUNT}.
+ *
+ * @author Andrey Khayrutdinov
+ */
+public class TopCountWithTwoParamsVersusHeadTest extends BatchTestCase {
+
+    private void assertResultsAreEqual(
+        Connection connection,
+        String testCase,
+            String topCountQuery)
+    {
+        if (!topCountQuery.contains("TOPCOUNT")) {
+            throw new IllegalArgumentException(
+                "'TOPCOUNT' was not found. Please ensure you are using upper case:\n\t\t"
+                    + topCountQuery);
+        }
+
+        String headQuery = topCountQuery.replace("TOPCOUNT", "HEAD");
+
+        Result topCountResult = executeQuery(topCountQuery, connection);
+        flushSchemaCache(connection);
+        Result headResult = executeQuery(headQuery, connection);
+        assertEquals(
+                TestUtil.toString(topCountResult),
+                TestUtil.toString(headResult),
+                String.format(
+                        "[%s]: TOPCOUNT() and HEAD() results of the query differ. The query:\n\t\t%s",
+                        testCase,
+                        topCountQuery));
+    }
+
+    @ParameterizedTest
+    @ContextSource(propertyUpdater = AppandFoodMartCatalogAsFile.class, dataloader = FastFoodmardDataLoader.class )
+    public void test_States(Context context) throws Exception {
+        assertResultsAreEqual(context.createConnection(),
+            "States",
+            TOPCOUNT_MIMICS_HEAD_WHEN_TWO_PARAMS_STATES_QUERY);
+    }
+
+    @ParameterizedTest
+    @ContextSource(propertyUpdater = AppandFoodMartCatalogAsFile.class, dataloader = FastFoodmardDataLoader.class )
+    public void test_Cities(Context context) throws Exception {
+        assertResultsAreEqual(context.createConnection(),
+            "Cities",
+            TOPCOUNT_MIMICS_HEAD_WHEN_TWO_PARAMS_CITIES_QUERY);
+    }
+
+    @ParameterizedTest
+    @ContextSource(propertyUpdater = AppandFoodMartCatalogAsFile.class, dataloader = FastFoodmardDataLoader.class )
+    public void test_ShowsNotMoreThanExist(Context context) {
+        assertResultsAreEqual(context.createConnection(),
+            "Not more than exists",
+            RESULTS_ARE_SHOWN_NOT_MORE_THAN_EXIST_2_PARAMS_QUERY);
+    }
+
+    @ParameterizedTest
+    @ContextSource(propertyUpdater = AppandFoodMartCatalogAsFile.class, dataloader = FastFoodmardDataLoader.class )
+    public void test_DoesNotIgnoreNonEmpty(Context context) {
+        assertResultsAreEqual(context.createConnection(),
+            "Does not ignore NON EMPTY",
+            NON_EMPTY_IS_NOT_IGNORED_WHEN_TWO_PARAMS_QUERY);
+    }
+}
+
+// End TopCountWithTwoParamsVersusHeadTest.java
