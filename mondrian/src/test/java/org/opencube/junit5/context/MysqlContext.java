e776be1ce4f10162cf4f9dd5f895e96b9a9a4dae
dbulahov <bulahovdenis@gmail.com>
2022-11-25 14:06:37 +0300

    fix license header Signed-off-by: dbulahov <bulahovdenis@gmail.com>

--- mondrian/src/test/java/org/opencube/junit5/context/MysqlContext.java
+++ mondrian/src/test/java/org/opencube/junit5/context/MysqlContext.java
@@ -1,6 +1,18 @@
+/*********************************************************************
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   SmartCity Jena - initial
+ *   Stefan Bischof (bipolis.org) - initial
+ **********************************************************************/
 package org.opencube.junit5.context;
 
-import mondrian.spi.impl.SqlStatisticsProvider;
 import org.eclipse.daanse.db.dialect.api.Dialect;
 import org.eclipse.daanse.db.dialect.db.mysql.MySqlDialect;
 import org.eclipse.daanse.db.statistics.api.StatisticsProvider;
