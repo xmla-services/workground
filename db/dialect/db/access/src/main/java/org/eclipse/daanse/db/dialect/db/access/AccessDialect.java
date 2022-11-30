0c76e0fb937e3240e12488310405430a103941cf
Stefan Bischof <stbischof@bipolis.org>
2022-11-30 15:01:31 +0100

    fix license
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- db/dialect/db/access/src/main/java/org/eclipse/daanse/db/dialect/db/access/AccessDialect.java
+++ db/dialect/db/access/src/main/java/org/eclipse/daanse/db/dialect/db/access/AccessDialect.java
@@ -1,10 +1,14 @@
 /*
-// This software is subject to the terms of the Eclipse Public License v1.0
-// Agreement, available at the following URL:
-// http://www.eclipse.org/legal/epl-v10.html.
-// You must accept the terms of that agreement to use this software.
-//
-// Copyright (c) 2002-2017 Hitachi Vantara..  All rights reserved.
+* This software is subject to the terms of the Eclipse Public License v1.0
+* Agreement, available at the following URL:
+* http://www.eclipse.org/legal/epl-v10.html.
+* You must accept the terms of that agreement to use this software.
+*
+* Copyright (C) 2012-2017 Hitachi Vantara and others
+* All Rights Reserved.
+* 
+* Contributors:
+*   SmartCity Jena, Stefan Bischof - make OSGi Component
 */
 package org.eclipse.daanse.db.dialect.db.access;
 
