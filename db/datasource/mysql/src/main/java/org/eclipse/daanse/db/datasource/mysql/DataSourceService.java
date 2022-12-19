b9d0b8853b660574819bf49b699e80293965cd93
Christoph Läubrich <laeubi@laeubi-soft.de>
2022-12-19 15:49:35 +0100

    Add ManagedServiceFactory with dynamic MetaType for MySql
    Signed-off-by: Christoph Läubrich <laeubi@laeubi-soft.de>

--- db/datasource/mysql/src/main/java/org/eclipse/daanse/db/datasource/mysql/DataSourceService.java
+++ db/datasource/mysql/src/main/java/org/eclipse/daanse/db/datasource/mysql/DataSourceService.java
@@ -13,58 +13,47 @@
 */
 package org.eclipse.daanse.db.datasource.mysql;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.util.Dictionary;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.sql.DataSource;
 
-import org.eclipse.daanse.db.datasource.common.AbstractDelegateDataSource;
-import org.osgi.service.component.annotations.Activate;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedServiceFactory;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.ServiceScope;
-import org.osgi.service.metatype.annotations.Designate;
-import org.osgi.util.converter.Converter;
-import org.osgi.util.converter.Converters;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
-@Designate(ocd = ConfigDataSource.class, factory = true)
-@Component(service = DataSource.class, scope = ServiceScope.SINGLETON)
-public class DataSourceService extends AbstractDelegateDataSource<MysqlDataSource> {
-    private static final Converter CONVERTER = Converters.standardConverter();
+@Component(service = ManagedServiceFactory.class, property = {Constants.SERVICE_PID+"=org.eclipse.daanse.db.datasource.mysql"})
+public class DataSourceService extends MysqlMetaTypeProviderService implements ManagedServiceFactory {
 
-    private ConfigConnectionPooledDataSource config;
-    private MysqlDataSource ds;
+	private Map<String, MysqlDataSourceRegistration<?>> registrations = new ConcurrentHashMap<>();
+	private BundleContext bundleContext;
 
-    @Activate
-    public DataSourceService(Map<String, Object> coniguration) throws SQLException {
-        this.ds = new MysqlDataSource();
-        this.config = CONVERTER.convert(coniguration)
-                .to(ConfigConnectionPooledDataSource.class);
+	public DataSourceService(BundleContext bundleContext) {
+		this.bundleContext = bundleContext;
+	}
 
-        ConfigUtil.configBase(ds, config);
-    }
+	@Override
+	public String getName() {
+		return "MySQL DataSourceService";
+	}
 
-    // no @Modified to force consumed Services get new configured connections.
+	@Override
+	public void updated(String pid, Dictionary<String, ?> properties) throws ConfigurationException {
+		MysqlDataSourceRegistration<?> registration = registrations.computeIfAbsent(pid,
+				nil -> new MysqlDataSourceRegistration<MysqlDataSource>(new MysqlDataSource(), DataSource.class));
+		registration.update(properties, bundleContext);
+	}
 
-    @Deactivate
-    public void deactivate() {
-
-        config = null;
-        ds=null;
-    }
-
-    @Override
-    public Connection getConnection() throws SQLException {
-
-        return super.getConnection(config.username(), config._password());
-    }
-
-    @Override
-    protected MysqlDataSource delegate() {
-        return ds;
-    }
-
+	@Override
+	public void deleted(String pid) {
+		MysqlDataSourceRegistration<?> registration = registrations.remove(pid);
+		if (registration != null) {
+			registration.dispose();
+		}
+	}
 }
