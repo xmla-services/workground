e1702342311f78252fabc2190e98951346937473
Stefan Bischof <stbischof@bipolis.org>
2023-01-31 19:38:02 +0100

    [xmla] doc cleanup
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- xmla/ws/jakarta.model/src/main/java/org/eclipse/daanse/xmla/ws/jakarta/model/xmla_rowset/Rowset.java
+++ xmla/ws/jakarta.model/src/main/java/org/eclipse/daanse/xmla/ws/jakarta/model/xmla_rowset/Rowset.java
@@ -24,98 +24,39 @@ import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlType;
 
-/**
- * <p>
- * Java class for rowset complex type.
- * 
- * <p>
- * The following schema fragment specifies the expected content contained within
- * this class.
- * 
- * <pre>
- * &lt;complexType name="rowset"&gt;
- *   &lt;complexContent&gt;
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
- *       &lt;sequence&gt;
- *         &lt;element name="row" type="{urn:schemas-microsoft-com:xml-analysis:rowset}row" maxOccurs="unbounded" minOccurs="0"/&gt;
- *         &lt;element name="Exception" type="{urn:schemas-microsoft-com:xml-analysis:exception}Exception" minOccurs="0"/&gt;
- *         &lt;element name="Messages" type="{urn:schemas-microsoft-com:xml-analysis:exception}Messages" minOccurs="0"/&gt;
- *       &lt;/sequence&gt;
- *     &lt;/restriction&gt;
- *   &lt;/complexContent&gt;
- * &lt;/complexType&gt;
- * </pre>
- * 
- * 
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "rowset", propOrder = { "row", "exception", "messages" })
 public class Rowset implements Serializable {
 
-  private final static long serialVersionUID = 1L;
-//  @XmlElement(name = "Row")
-  protected List<Row> row;
-  @XmlElement(name = "Exception")
-  protected Exception exception;
-  @XmlElement(name = "Messages")
-  protected Messages messages;
+    private final static long serialVersionUID = 1L;
+    protected List<Row> row;
+    @XmlElement(name = "Exception")
+    protected Exception exception;
+    @XmlElement(name = "Messages")
+    protected Messages messages;
 
+    public List<Row> getRow() {
+        return row;
+    }
 
-  public List<Row> getRow() {
-      return row;
-  }
+    public void setRow(List<Row> row) {
+        this.row = row;
+    }
 
-  public void setRow(List<Row> row) {
-      this.row = row;
-  }
+    public Exception getException() {
+        return exception;
+    }
 
+    public void setException(Exception value) {
+        this.exception = value;
+    }
 
-  /**
-   * Gets the value of the exception property.
-   * 
-   * @return possible object is {@link Exception }
-   * 
-   */
-  public Exception getException() {
-    return exception;
-  }
+    public Messages getMessages() {
+        return messages;
+    }
 
-  /**
-   * Sets the value of the exception property.
-   * 
-   * @param value allowed object is {@link Exception }
-   * 
-   */
-  public void setException(Exception value) {
-    this.exception = value;
-  }
-
-  public boolean isSetException() {
-    return (this.exception != null);
-  }
-
-  /**
-   * Gets the value of the messages property.
-   * 
-   * @return possible object is {@link Messages }
-   * 
-   */
-  public Messages getMessages() {
-    return messages;
-  }
-
-  /**
-   * Sets the value of the messages property.
-   * 
-   * @param value allowed object is {@link Messages }
-   * 
-   */
-  public void setMessages(Messages value) {
-    this.messages = value;
-  }
-
-  public boolean isSetMessages() {
-    return (this.messages != null);
-  }
+    public void setMessages(Messages value) {
+        this.messages = value;
+    }
 
 }
