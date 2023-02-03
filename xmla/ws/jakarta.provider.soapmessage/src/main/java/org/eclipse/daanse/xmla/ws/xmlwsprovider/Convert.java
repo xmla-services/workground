9405093d20e552ae96ea1c032e54e1a5314f335b
Stefan Bischof <stbischof@bipolis.org>
2023-02-03 20:56:44 +0100

    [xmla] init additional xmla.ws using provider
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ xmla/ws/jakarta.provider.soapmessage/src/main/java/org/eclipse/daanse/xmla/ws/xmlwsprovider/Convert.java
@@ -0,0 +1,36 @@
+/*
+* Copyright (c) 2023 Contributors to the Eclipse Foundation.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   SmartCity Jena - initial
+*   Stefan Bischof (bipolis.org) - initial
+*/
+package org.eclipse.daanse.xmla.ws.xmlwsprovider;
+
+import java.util.List;
+
+import org.eclipse.daanse.xmla.api.discover.discoverproperties.DiscoverPropertiesResponseRow;
+import org.eclipse.daanse.xmla.model.record.discover.discoverproperties.DiscoverPropertiesRestrictionsR;
+
+import jakarta.xml.soap.Node;
+import jakarta.xml.soap.SOAPBody;
+
+public class Convert {
+
+    public static DiscoverPropertiesRestrictionsR discoverPropertiesRestrictions(Node restriction) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static SOAPBody toDiscoverProperties(List<DiscoverPropertiesResponseRow> rows) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
