a228e3b92a839c7bec7f07b988c3c711ebcb4d60
Stefan Bischof <stbischof@bipolis.org>
2023-02-14 19:13:49 +0100

    [xmla] create demo server module
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ xmla/demo/server/src/main/java/org/eclipse/daanse/xmla/demo/server/DemoExecuteService.java
@@ -0,0 +1,7 @@
+package org.eclipse.daanse.xmla.demo.server;
+
+import org.eclipse.daanse.xmla.api.execute.ExecuteService;
+
+public class DemoExecuteService implements ExecuteService {
+
+}
---
+++ xmla/demo/server/src/main/java/org/eclipse/daanse/xmla/demo/server/DemoXmlaService.java
@@ -0,0 +1,30 @@
+package org.eclipse.daanse.xmla.demo.server;
+
+import org.eclipse.daanse.xmla.api.XmlaService;
+import org.eclipse.daanse.xmla.api.discover.DiscoverService;
+import org.eclipse.daanse.xmla.api.execute.ExecuteService;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.propertytypes.ServiceVendor;
+
+@Component(service = XmlaService.class, immediate = true)
+@ServiceVendor(value = "demo")
+public class DemoXmlaService implements XmlaService {
+
+    public DemoXmlaService() {
+        System.out.println(1);
+    }
+
+    private DiscoverService discoverService = new DemoDiscoverService();
+    private ExecuteService executeService = new DemoExecuteService();
+
+    @Override
+    public DiscoverService discover() {
+        return discoverService;
+    }
+
+    @Override
+    public ExecuteService execute() {
+        return executeService;
+    }
+
+}
