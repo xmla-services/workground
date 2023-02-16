5cb56632c99e868bc5f753d40ae599e57487e1c7
Stefan Bischof <stbischof@bipolis.org>
2023-02-16 15:31:39 +0100

    [xmla] cleanup generated javadoc, rm isSetMethods, handle Arrays only with getter and setter
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- xmla/ws/jakarta.model.xmla/src/main/java/org/eclipse/daanse/xmla/ws/jakarta/model/xmla/xmla_mddataset/Axis.java
+++ xmla/ws/jakarta.model.xmla/src/main/java/org/eclipse/daanse/xmla/ws/jakarta/model/xmla/xmla_mddataset/Axis.java
@@ -13,7 +13,6 @@
 */
 package org.eclipse.daanse.xmla.ws.jakarta.model.xmla.xmla_mddataset;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.daanse.xmla.ws.jakarta.model.xmla.msxmla.NormTupleSet;
@@ -25,101 +24,34 @@ import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlElements;
 import jakarta.xml.bind.annotation.XmlType;
 
-/**
- * <p>
- * Java class for Axis complex type.
- * 
- * <p>
- * The following schema fragment specifies the expected content contained within
- * this class.
- * 
- * <pre>
- * &lt;complexType name="Axis"&gt;
- *   &lt;complexContent&gt;
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
- *       &lt;group ref="{urn:schemas-microsoft-com:xml-analysis:mddataset}SetType" maxOccurs="unbounded" minOccurs="0"/&gt;
- *       &lt;attribute name="name" type="{http://www.w3.org/2001/XMLSchema}string" /&gt;
- *     &lt;/restriction&gt;
- *   &lt;/complexContent&gt;
- * &lt;/complexType&gt;
- * </pre>
- * 
- * 
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "Axis", propOrder = { "setType" })
 public class Axis {
 
-  @XmlElements({ @XmlElement(name = "Members", type = MembersType.class),
-      @XmlElement(name = "Tuples", type = TuplesType.class),
-      @XmlElement(name = "CrossProduct", type = SetListType.class),
-      @XmlElement(name = "NormTupleSet", namespace = "http://schemas.microsoft.com/analysisservices/2003/xmla", type = NormTupleSet.class),
-      @XmlElement(name = "Union", type = Union.class) })
-  protected List<Object> setType;
-  @XmlAttribute(name = "name")
-  protected String name;
+    @XmlElements({ @XmlElement(name = "Members", type = MembersType.class),
+            @XmlElement(name = "Tuples", type = TuplesType.class),
+            @XmlElement(name = "CrossProduct", type = SetListType.class),
+            @XmlElement(name = "NormTupleSet", namespace = "http://schemas.microsoft.com/analysisservices/2003/xmla", type = NormTupleSet.class),
+            @XmlElement(name = "Union", type = Union.class) })
+    protected List<Object> setType;
+    @XmlAttribute(name = "name")
+    protected String name;
 
-  /**
-   * Gets the value of the setType property.
-   * 
-   * <p>
-   * This accessor method returns a reference to the live list, not a snapshot.
-   * Therefore any modification you make to the returned list will be present
-   * inside the Jakarta XML Binding object. This is why there is not a
-   * <CODE>set</CODE> method for the setType property.
-   * 
-   * <p>
-   * For example, to add a new item, do as follows:
-   * 
-   * <pre>
-   * getSetType().add(newItem);
-   * </pre>
-   * 
-   * 
-   * <p>
-   * Objects of the following type(s) are allowed in the list {@link NormTupleSet
-   * } {@link MembersType } {@link SetListType } {@link SetListType.Union }
-   * {@link TuplesType }
-   * 
-   * 
-   */
-  public List<Object> getSetType() {
-    if (setType == null) {
-      setType = new ArrayList<Object>();
+    public List<Object> getSetType() {
+
+        return this.setType;
     }
-    return this.setType;
-  }
 
-  public boolean isSetSetType() {
-    return ((this.setType != null) && (!this.setType.isEmpty()));
-  }
+    public void setSetType(List<Object> setType) {
+        this.setType = setType;
+    }
 
-  public void unsetSetType() {
-    this.setType = null;
-  }
+    public String getName() {
+        return name;
+    }
 
-  /**
-   * Gets the value of the name property.
-   * 
-   * @return possible object is {@link String }
-   * 
-   */
-  public String getName() {
-    return name;
-  }
-
-  /**
-   * Sets the value of the name property.
-   * 
-   * @param value allowed object is {@link String }
-   * 
-   */
-  public void setName(String value) {
-    this.name = value;
-  }
-
-  public boolean isSetName() {
-    return (this.name != null);
-  }
+    public void setName(String value) {
+        this.name = value;
+    }
 
 }
