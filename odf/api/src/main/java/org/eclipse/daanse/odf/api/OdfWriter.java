719403bebeff5eee8840bd2b3ac864f458c8671f
Stefan Bischof <stbischof@bipolis.org>
2023-02-28 21:41:49 +0100

    evaluating open document spreadsheet
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ odf/api/src/main/java/org/eclipse/daanse/odf/api/OdfWriter.java
@@ -0,0 +1,12 @@
+package org.eclipse.daanse.odf.api;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Path;
+
+public interface OdfWriter {
+
+    void write(Path targetFilePath, SpreadSheet spreadSheet) throws  IOException;
+
+
+}
---
+++ odf/impl.basic/src/test/java/org/eclipse/daanse/odf/impl/basic/BaseOdfWriterTest.java
@@ -0,0 +1,35 @@
+package org.eclipse.daanse.odf.impl.basic;
+
+import static org.mockito.Mockito.mock;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.eclipse.daanse.odf.api.OdfWriter;
+import org.eclipse.daanse.odf.api.SpreadSheet;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+public class BaseOdfWriterTest {
+
+    @TempDir
+    Path tempPath;
+
+    Path file;
+
+    @BeforeEach
+    void beforeEach() throws IOException {
+        file = Files.createTempFile(tempPath, "test", "ods");
+
+    }
+
+    @Test
+    void testName() throws Exception {
+        SpreadSheet spreadSheet = mock(SpreadSheet.class);
+        OdfWriter writer = new BaseOdfWriter();
+        writer.write(file, spreadSheet);
+    }
+}
