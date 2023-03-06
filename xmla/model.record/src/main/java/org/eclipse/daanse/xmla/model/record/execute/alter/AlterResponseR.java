d3389fed7b8a54e7b36e323f40b5de32b543dadf
dbulahov <bulahovdenis@gmail.com>
2023-03-06 14:28:48 +0300

    execute alter request response test Signed-off-by: dbulahov <bulahovdenis@gmail.com>

---
+++ xmla/model.record/src/main/java/org/eclipse/daanse/xmla/model/record/execute/alter/AlterResponseR.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   SmartCity Jena - initial
+ *   Stefan Bischof (bipolis.org) - initial
+ */
+package org.eclipse.daanse.xmla.model.record.execute.alter;
+
+import org.eclipse.daanse.xmla.api.execute.alter.AlterResponse;
+import org.eclipse.daanse.xmla.api.xmla_empty.Emptyresult;
+
+public record AlterResponseR(Emptyresult emptyresult) implements AlterResponse {
+
+}
