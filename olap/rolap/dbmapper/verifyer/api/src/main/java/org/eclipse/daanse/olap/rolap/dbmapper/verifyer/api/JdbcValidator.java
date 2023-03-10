1d53fb851c07c14dfb16536e087aaa64ba33c8a5
dbulahov <bulahovdenis@gmail.com>
2023-03-10 14:38:00 +0300

    databaseVerifyer implementation Signed-off-by: dbulahov <bulahovdenis@gmail.com>

---
+++ olap/rolap/dbmapper/verifyer/api/src/main/java/org/eclipse/daanse/olap/rolap/dbmapper/verifyer/api/JdbcValidator.java
@@ -0,0 +1,46 @@
+/*
+* This software is subject to the terms of the Eclipse Public License v1.0
+* Agreement, available at the following URL:
+* http://www.eclipse.org/legal/epl-v10.html.
+* You must accept the terms of that agreement to use this software.
+*
+* Copyright (c) 2002-2017 Hitachi Vantara..  All rights reserved.
+*/
+
+package org.eclipse.daanse.olap.rolap.dbmapper.verifyer.api;
+
+/**
+ * Validation for database schema, table, and columns. Extracted interface from
+ * <code>mondrian.gui.JDBCMetaData</code>.
+ *
+ * @author mlowery
+ */
+public interface JdbcValidator {
+    /**
+     * Returns the data type of given column.
+     *
+     * @return SQL type from java.sql.Types
+     */
+    int getColumnDataType(String schemaName, String tableName, String colName);
+
+    /**
+     * Returns true if column exists.
+     */
+    boolean isColExists(String schemaName, String tableName, String colName);
+
+    /**
+     * Returns true if table exists.
+     */
+    boolean isTableExists(String schemaName, String tableName);
+
+    /**
+     * Returns true if this object successfully connected to database (and
+     * validation methods can now be called).
+     */
+    boolean isInitialized();
+
+    /**
+     * Returns true if schema exists.
+     */
+    boolean isSchemaExists(String schemaName);
+}
