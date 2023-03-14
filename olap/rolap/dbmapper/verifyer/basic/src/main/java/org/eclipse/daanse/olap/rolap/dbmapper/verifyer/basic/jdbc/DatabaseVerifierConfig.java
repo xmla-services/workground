84a880d2e784330216ef01d0acd7e82be761ffce
Stefan Bischof <stbischof@bipolis.org>
2023-03-14 21:16:27 +0100

    [verifyer] refactor package, make Description RequireConfig
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ olap/rolap/dbmapper/verifyer/basic/src/main/java/org/eclipse/daanse/olap/rolap/dbmapper/verifyer/basic/jdbc/DatabaseVerifierConfig.java
@@ -0,0 +1,8 @@
+package org.eclipse.daanse.olap.rolap.dbmapper.verifyer.basic.jdbc;
+
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+@ObjectClassDefinition
+public interface DatabaseVerifierConfig {
+
+}
