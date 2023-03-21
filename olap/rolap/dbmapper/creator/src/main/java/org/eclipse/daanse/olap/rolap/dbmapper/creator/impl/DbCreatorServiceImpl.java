44d0cd6e3d0990c657bfc01e21a9299496023296
dbulahov <bulahovdenis@gmail.com>
2023-03-21 11:53:59 +0300

    create Database creator modules Signed-off-by: dbulahov <bulahovdenis@gmail.com>

---
+++ olap/rolap/dbmapper/creator/src/main/java/org/eclipse/daanse/olap/rolap/dbmapper/creator/impl/DbCreatorServiceImpl.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   SmartCity Jena, Stefan Bischof - initial
+ *
+ */
+package org.eclipse.daanse.olap.rolap.dbmapper.creator.impl;
+
+import org.eclipse.daanse.olap.rolap.dbmapper.creator.api.DbCreatorService;
+
+import javax.sql.DataSource;
+import java.io.InputStream;
+
+public class DbCreatorServiceImpl  implements DbCreatorService {
+
+    public DbCreatorServiceImpl(DataSource dataSource) {
+    }
+
+    @Override
+    public void loadData(InputStream inputStream) {
+        //TODO
+    }
+
+    @Override
+    public void createSchema(InputStream inputStream) {
+        //TODO
+    }
+}
