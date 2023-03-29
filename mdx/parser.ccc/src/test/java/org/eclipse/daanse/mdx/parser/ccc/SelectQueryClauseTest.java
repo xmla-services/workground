da1ee37550b97d4a00c8d145d04e4791b5817f42
dbulahov <bulahovdenis@gmail.com>
2023-03-29 12:04:03 +0300

    mdx tests Signed-off-by: dbulahov <bulahovdenis@gmail.com>

--- mdx/parser.ccc/src/test/java/org/eclipse/daanse/mdx/parser/ccc/SelectQueryClauseTest.java
+++ mdx/parser.ccc/src/test/java/org/eclipse/daanse/mdx/parser/ccc/SelectQueryClauseTest.java
@@ -13,14 +13,14 @@
 */
 package org.eclipse.daanse.mdx.parser.ccc;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.eclipse.daanse.mdx.model.api.SelectStatement;
 import org.eclipse.daanse.mdx.model.api.expression.CallExpression;
 import org.eclipse.daanse.mdx.model.api.expression.Expression;
+import org.eclipse.daanse.mdx.model.api.expression.NameObjectIdentifier;
 import org.eclipse.daanse.mdx.model.api.select.SelectQueryAsteriskClause;
 import org.eclipse.daanse.mdx.model.api.select.SelectQueryAxesClause;
 import org.eclipse.daanse.mdx.model.record.expression.CallExpressionR;
+import org.eclipse.daanse.mdx.model.record.expression.CompoundIdR;
 import org.eclipse.daanse.mdx.model.record.select.SelectQueryAsteriskClauseR;
 import org.eclipse.daanse.mdx.model.record.select.SelectQueryAxesClauseR;
 import org.eclipse.daanse.mdx.model.record.select.SelectQueryEmptyClauseR;
@@ -29,6 +29,8 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SelectQueryClauseTest {
 
@@ -106,77 +108,166 @@ public class SelectQueryClauseTest {
             assertThat(clause).isNotNull();
         }
 
-        @ParameterizedTest
-        @ValueSource(strings = { "FunctionName()", "FunctionName(arg)", "FunctionName([arg1, arg2])"})
-        public void testCallExpressionFunction(String mdx) throws MdxParserException {
+        @Test
+        public void testCallExpressionFunctionWithArrayParam() throws MdxParserException {
+        	String mdx = "FunctionName([arg1, arg2])";
             Expression clause = new MdxParserWrapper(mdx).parseExpression();
             assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
             assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.Function);
+            assertThat(((CallExpressionR)clause).name()).isEqualTo("FunctionName");
+            assertThat(((CallExpressionR)clause).expressions()).hasSize(1);
+            checkArgument((CallExpressionR)clause, 0, "arg1, arg2");
         }
 
-        @ParameterizedTest
-        @ValueSource(strings = {"object.PROPERTY"})
-        public void testCallExpressionProperty(String mdx) throws MdxParserException {
+        @Test
+        public void testCallExpressionFunctionWithoutParams() throws MdxParserException {
+        	String mdx = "FunctionName()";
+            Expression clause = new MdxParserWrapper(mdx).parseExpression();
+            assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
+            assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.Function);
+            assertThat(((CallExpressionR)clause).name()).isEqualTo("FunctionName");
+            assertThat(((CallExpressionR)clause).expressions()).hasSize(0);
+        }
+
+        @Test
+        public void testCallExpressionFunctionWithOneParam() throws MdxParserException {
+            String mdx = "FunctionName(arg)";
+            Expression clause = new MdxParserWrapper(mdx).parseExpression();
+            assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
+            assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.Function);
+            assertThat(((CallExpressionR)clause).name()).isEqualTo("FunctionName");
+            assertThat(((CallExpressionR)clause).expressions()).hasSize(1);
+            checkArgument((CallExpressionR)clause, 0, "arg");
+        }
+
+        @Test
+        public void testCallExpressionFunctionWithSeveralParams() throws MdxParserException {
+            String mdx = "FunctionName(arg1, arg2)";
+            Expression clause = new MdxParserWrapper(mdx).parseExpression();
+            assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
+            assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.Function);
+            assertThat(((CallExpressionR)clause).name()).isEqualTo("FunctionName");
+            assertThat(((CallExpressionR)clause).expressions()).hasSize(2);
+            checkArgument((CallExpressionR)clause, 0, "arg1");
+            checkArgument((CallExpressionR)clause, 1, "arg2");
+        }
+
+        @Test
+        public void testCallExpressionFunctionWithSeveralParamsWithArray() throws MdxParserException {
+            String mdx = "FunctionName(arg1, [arg2, arg3])";
+            Expression clause = new MdxParserWrapper(mdx).parseExpression();
+            assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
+            assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.Function);
+            assertThat(((CallExpressionR)clause).name()).isEqualTo("FunctionName");
+            assertThat(((CallExpressionR)clause).expressions()).hasSize(2);
+            checkArgument((CallExpressionR)clause, 0, "arg1");
+            checkArgument((CallExpressionR)clause, 1, "arg2, arg3");
+        }
+
+        @Test
+        public void testCallExpressionProperty() throws MdxParserException {
+        	String mdx = "object.PROPERTY";
             Expression clause = new MdxParserWrapper(mdx).parseExpression();
             assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
             assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.Property);
+            assertThat(((CallExpressionR)clause).name()).isEqualTo("PROPERTY");
+            assertThat(((CallExpressionR)clause).expressions()).hasSize(1);
+            checkArgument((CallExpressionR)clause, 0, "object");
         }
 
-        @ParameterizedTest
-        @ValueSource(strings = {"object.&PROPERTY"})
-        public void testCallExpressionPropertyQuoted(String mdx) throws MdxParserException {
+        @Test
+        public void testCallExpressionPropertyQuoted() throws MdxParserException {
+        	String mdx = "object.&PROPERTY";
             Expression clause = new MdxParserWrapper(mdx).parseExpression();
             assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
             assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.PropertyQuoted);
         }
 
-        @ParameterizedTest
-        @ValueSource(strings = {"object.[&PROPERTY"})
-        public void testCallExpressionPropertyAmpersAndQuoted(String mdx) throws MdxParserException {
+        @Test
+        public void testCallExpressionPropertyAmpersAndQuoted() throws MdxParserException {
+        	String mdx = "object.[&PROPERTY";
             Expression clause = new MdxParserWrapper(mdx).parseExpression();
             assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
             assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.PropertyAmpersAndQuoted);
         }
 
         @ParameterizedTest
-        @ValueSource(strings = {"object.FunctionName()", "object.FunctionName([arg1, arg2])", "object.FunctionName(arg)"})
+        @ValueSource(strings = {"object.FunctionName()"})
         public void testCallExpressionMethod(String mdx) throws MdxParserException {
             Expression clause = new MdxParserWrapper(mdx).parseExpression();
             assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
             assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.Method);
+            assertThat(((CallExpressionR)clause).name()).isEqualTo("FunctionName");
+        }
+        
+        @Test        
+        public void testCallExpressionMethodWithParameter() throws MdxParserException {
+        	String mdx = "object.FunctionName(arg)";
+            Expression clause = new MdxParserWrapper(mdx).parseExpression();
+            assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
+            assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.Method);
+            assertThat(((CallExpressionR)clause).name()).isEqualTo("FunctionName");
+            assertThat(((CallExpressionR)clause).expressions()).hasSize(2);
+            checkArgument((CallExpressionR)clause, 0, "object");
+            checkArgument((CallExpressionR)clause, 1, "arg");
         }
 
-        @ParameterizedTest
-        @ValueSource(strings = {"CASE a WHEN b THEN c END"})
-        public void testCallExpressionTermCase(String mdx) throws MdxParserException {
+        @Test        
+        public void testCallExpressionMethodWithParameterArray() throws MdxParserException {
+        	String mdx = "object.FunctionName([arg1, arg2])";
+            Expression clause = new MdxParserWrapper(mdx).parseExpression();
+            assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
+            assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.Method);
+            assertThat(((CallExpressionR)clause).name()).isEqualTo("FunctionName");
+            assertThat(((CallExpressionR)clause).expressions()).hasSize(2);
+            checkArgument((CallExpressionR)clause, 0, "object");
+            checkArgument((CallExpressionR)clause, 1, "arg1, arg2");
+        }
+
+        
+        @Test
+        public void testCallExpressionTermCase() throws MdxParserException {
+        	String mdx = "CASE a WHEN b THEN c END";
             Expression clause = new MdxParserWrapper(mdx).parseExpression();
             assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
             assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.Term_Case);
+            assertThat(((CallExpressionR)clause).name()).isEqualTo("_CaseMatch");
+            assertThat(((CallExpressionR)clause).expressions()).hasSize(3);            
         }
 
-        @ParameterizedTest
-        @ValueSource(strings = {"{ expression } { expression,expression } { [a][a] : [a][c] } { [a][a], [a][b], [a][c] }"})
-        public void testCallExpressionBraces(String mdx) throws MdxParserException {
+        @Test
+        public void testCallExpressionBraces() throws MdxParserException {
+        	String mdx = "{ expression } { expression,expression } { [a][a] : [a][c] } { [a][a], [a][b], [a][c] }";
             Expression clause = new MdxParserWrapper(mdx).parseExpression();
             assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
             assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.Braces);
+            assertThat(((CallExpressionR)clause).name()).isEqualTo("{}");
         }
 
-        @ParameterizedTest
-        @ValueSource(strings = {"( arg, arg )"})
-        public void testCallExpressionParentheses(String mdx) throws MdxParserException {
+        @Test
+        public void testCallExpressionParentheses() throws MdxParserException {
+        	String mdx = "( arg, arg )";
             Expression clause = new MdxParserWrapper(mdx).parseExpression();
             assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
             assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.Parentheses);
+            assertThat(((CallExpressionR)clause).name()).isEqualTo("()");
         }
 
-        @ParameterizedTest
-        @ValueSource(strings = {"arg IS EMPTY"})
-        public void testCallExpressionTermPostfix(String mdx) throws MdxParserException {
+        @Test
+        public void testCallExpressionTermPostfix() throws MdxParserException {
+        	String mdx = "arg IS EMPTY";
             Expression clause = new MdxParserWrapper(mdx).parseExpression();
             assertThat(clause).isNotNull().isInstanceOf(CallExpressionR.class);
             assertThat(((CallExpressionR)clause).type()).isEqualTo(CallExpression.Type.Term_Postfix);
+            assertThat(((CallExpressionR)clause).name()).isEqualTo("IS EMPTY");
         }
 
+        private void checkArgument(CallExpressionR clause, int index, String arg) {
+            assertThat(clause.expressions().get(index)).isInstanceOf(CompoundIdR.class);
+            CompoundIdR compoundId = (CompoundIdR)(clause.expressions().get(index));
+            assertThat(compoundId.objectIdentifiers()).isNotNull().hasSize(1);
+            assertThat(compoundId.objectIdentifiers().get(0)).isInstanceOf(NameObjectIdentifier.class);
+            assertThat(((NameObjectIdentifier)(compoundId.objectIdentifiers().get(0))).name()).isEqualTo(arg);
+        }
     }
 }
