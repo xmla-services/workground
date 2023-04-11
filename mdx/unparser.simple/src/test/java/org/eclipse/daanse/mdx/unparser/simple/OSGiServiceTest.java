afa8197fbd9c3bcdb7d382cc10030db5c23af8a5
Stefan Bischof <stbischof@bipolis.org>
2023-04-11 14:22:23 +0200

    [mdx] add test for unparser
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ mdx/unparser.simple/src/test/java/org/eclipse/daanse/mdx/unparser/simple/OSGiServiceTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   SmartCity Jena - initial
+ *   Stefan Bischof (bipolis.org) - initial
+ */
+package org.eclipse.daanse.mdx.unparser.simple;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.eclipse.daanse.mdx.unparser.api.UnParser;
+import org.junit.jupiter.api.Test;
+import org.osgi.service.component.annotations.RequireServiceComponentRuntime;
+import org.osgi.test.common.annotation.InjectService;
+
+@RequireServiceComponentRuntime
+class OSGiServiceTest {
+
+	@Test
+	void testSelectStatement(@InjectService UnParser unParser) throws Exception {
+
+		assertThat(unParser).isNotNull().isInstanceOf(SimpleUnparser.class);
+	}
+
+}
