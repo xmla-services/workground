666b3a1a280275a084736df35273abbc17fa2ca2
dbulahov <bulahovdenis@gmail.com>
2023-06-05 17:46:38 +0300

    Schema creator service from database Signed-off-by: dbulahov <bulahovdenis@gmail.com>

---
+++ db/jdbc/metadata/src/main/java/org/eclipse/daanse/db/jdbc/metadata/impl/Column.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   SmartCity Jena, Stefan Bischof - initial
+ *
+ */
+package org.eclipse.daanse.db.jdbc.metadata.impl;
+
+public class Column {
+
+    private String name;
+    private Integer type;
+
+    public Column(String name, Integer type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+}
--- db/jdbc/metadata/src/main/java/org/eclipse/daanse/db/jdbc/metadata/impl/ForeignKey.java
+++ db/jdbc/metadata/src/main/java/org/eclipse/daanse/db/jdbc/metadata/impl/ForeignKey.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   SmartCity Jena, Stefan Bischof - initial
+ *
+ */
 package org.eclipse.daanse.db.jdbc.metadata.impl;
 
 public class ForeignKey {
