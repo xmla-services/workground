6c55b2833eeedc991972fa31dfd37a7811d4214e
dbulahov <bulahovdenis@gmail.com>
2023-06-14 18:20:20 +0300

    xmla client implementation Signed-off-by: dbulahov <bulahovdenis@gmail.com>

---
+++ xmla/client/soapmessage/src/main/java/org/eclipse/daanse/xmla/client/soapmessage/SoapClientException.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   SmartCity Jena - initial
+ *   Stefan Bischof (bipolis.org) - initial
+ */
+package org.eclipse.daanse.xmla.client.soapmessage;
+
+import jakarta.xml.soap.SOAPException;
+
+public class SoapClientException extends RuntimeException {
+
+    public SoapClientException(String msg, SOAPException e) {
+        super(msg, e);
+    }
+
+    public SoapClientException(String msg) {
+        super(msg);
+    }
+}
--- xmla/ws/jakarta.model.xmla/src/main/java/org/eclipse/daanse/xmla/ws/jakarta/model/xmla/xmla_rowset/ParameterInfoXml.java
+++ xmla/ws/jakarta.model.xmla/src/main/java/org/eclipse/daanse/xmla/ws/jakarta/model/xmla/xmla_rowset/ParameterInfoXml.java
@@ -15,6 +15,7 @@ package org.eclipse.daanse.xmla.ws.jakarta.model.xmla.xmla_rowset;
 
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlTransient;
 import jakarta.xml.bind.annotation.XmlType;
 
@@ -26,14 +27,36 @@ public class ParameterInfoXml implements Serializable {
     @XmlTransient
     private static final long serialVersionUID = -2141241183459860742L;
 
+    /**
+     * The name of the parameter.
+     */
+    @XmlElement(name = "NAME")
     private String name;
 
+    /**
+     * The description of the parameter.
+     */
+    @XmlElement(name = "DESCRIPTION")
     private String description;
 
+    /**
+     * A Boolean that, when true, indicates that the parameter is
+     * optional.
+     */
+    @XmlElement(name = "OPTIONAL")
     private Boolean optional;
 
+    /**
+     * A Boolean that, when true, indicates that multiple values can be
+     * specified for this parameter.
+     */
+    @XmlElement(name = "REPEATABLE")
     private Boolean repeatable;
 
+    /**
+     * The index of the repeat group of this parameter.
+     */
+    @XmlElement(name = "REPEATGROUP")
     private Integer repeatGroup;
 
     public String getName() {
