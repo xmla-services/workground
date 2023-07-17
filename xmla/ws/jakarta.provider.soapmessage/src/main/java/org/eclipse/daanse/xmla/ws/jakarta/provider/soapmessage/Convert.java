c1038c108ddbafef3c2297fb0f1f07f8473543aa
dbulahov <bulahovdenis@gmail.com>
2023-07-17 13:55:03 +0300

    jakarta provider soap message Signed-off-by: dbulahov <bulahovdenis@gmail.com>

--- xmla/ws/jakarta.provider.soapmessage/src/main/java/org/eclipse/daanse/xmla/ws/jakarta/provider/soapmessage/Convert.java
+++ xmla/ws/jakarta.provider.soapmessage/src/main/java/org/eclipse/daanse/xmla/ws/jakarta/provider/soapmessage/Convert.java
@@ -13,28 +13,48 @@
 */
 package org.eclipse.daanse.xmla.ws.jakarta.provider.soapmessage;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
-
-import org.eclipse.daanse.xmla.api.common.properties.PropertyListElementDefinition;
-import org.eclipse.daanse.xmla.api.discover.discover.properties.DiscoverPropertiesResponseRow;
-import org.eclipse.daanse.xmla.model.record.discover.PropertiesR;
-import org.eclipse.daanse.xmla.model.record.discover.discover.properties.DiscoverPropertiesRestrictionsR;
-
+import jakarta.xml.soap.MessageFactory;
 import jakarta.xml.soap.Node;
 import jakarta.xml.soap.SOAPBody;
 import jakarta.xml.soap.SOAPElement;
+import jakarta.xml.soap.SOAPException;
+import jakarta.xml.soap.SOAPMessage;
+import org.eclipse.daanse.xmla.api.common.enums.CubeSourceEnum;
+import org.eclipse.daanse.xmla.api.common.enums.InterfaceNameEnum;
+import org.eclipse.daanse.xmla.api.common.enums.OriginEnum;
+import org.eclipse.daanse.xmla.api.common.properties.PropertyListElementDefinition;
+import org.eclipse.daanse.xmla.api.discover.discover.properties.DiscoverPropertiesResponseRow;
+import org.eclipse.daanse.xmla.api.discover.mdschema.cubes.MdSchemaCubesResponseRow;
+import org.eclipse.daanse.xmla.api.discover.mdschema.demensions.MdSchemaDimensionsResponseRow;
+import org.eclipse.daanse.xmla.api.discover.mdschema.functions.MdSchemaFunctionsResponseRow;
+import org.eclipse.daanse.xmla.model.record.discover.PropertiesR;
+import org.eclipse.daanse.xmla.model.record.discover.discover.properties.DiscoverPropertiesRestrictionsR;
+import org.eclipse.daanse.xmla.model.record.discover.mdschema.cubes.MdSchemaCubesRestrictionsR;
+import org.eclipse.daanse.xmla.model.record.discover.mdschema.demensions.MdSchemaDimensionsRestrictionsR;
+import org.eclipse.daanse.xmla.model.record.discover.mdschema.functions.MdSchemaFunctionsRestrictionsR;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.NodeList;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class Convert {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Convert.class);
+    public static final String ROW = "row";
 
     private Convert() {
         // constructor
     }
 
     public static DiscoverPropertiesRestrictionsR discoverPropertiesRestrictions(SOAPElement restriction) {
-        // TODO Auto-generated method stub
-        return null;
+        Map<String, String> m = getMapValuesByTag(restriction, "RestrictionList");
+        return new DiscoverPropertiesRestrictionsR(
+            Optional.ofNullable(m.get("PropertyName"))
+        );
     }
 
     public static PropertiesR propertiestoProperties(SOAPElement propertiesElement) {
@@ -44,7 +64,7 @@ public class Convert {
             Node node = nodeIterator.next();
             if (node instanceof SOAPElement propertyList
                 && Constants.QNAME_MSXMLA_PROPERTYLIST.equals(propertyList.getElementQName())) {
-                return propertyListToproperties(propertyList);
+                return propertyListToProperties(propertyList);
             }
 
         }
@@ -52,7 +72,7 @@ public class Convert {
 
     }
 
-    private static PropertiesR propertyListToproperties(SOAPElement propertyList) {
+    private static PropertiesR propertyListToProperties(SOAPElement propertyList) {
         PropertiesR properties = new PropertiesR();
 
         Iterator<Node> nodeIteratorPropertyList = propertyList.getChildElements();
@@ -69,8 +89,113 @@ public class Convert {
     }
 
     public static SOAPBody toDiscoverProperties(List<DiscoverPropertiesResponseRow> rows) {
+        SOAPBody body = createSOAPBody();
+        SOAPElement root = addRoot(body);
+        rows.forEach(r -> {
+            addDiscoverPropertiesResponseRow(root, r);
+        });
+
+        return body;
+    }
+
+    private static void addDiscoverPropertiesResponseRow(SOAPElement root, DiscoverPropertiesResponseRow r) {
+        SOAPElement row = addChildElement(root, ROW);
+        addChildElement(root, "PropertyName", r.propertyName());
+        r.propertyDescription().ifPresent(v -> addChildElement(root, "PropertyDescription", v));
+        r.propertyDescription().ifPresent(v -> addChildElement(root, "PropertyType", v));
+        addChildElement(root, "PropertyAccessType", r.propertyAccessType());
+        r.required().ifPresent(v -> addChildElement(root, "IsRequired", String.valueOf(v)));
+        r.value().ifPresent(v -> addChildElement(root, "Value", v));
+    }
+
+    private static SOAPBody createSOAPBody() {
+        try {
+            SOAPMessage message = MessageFactory.newInstance().createMessage();
+            return message.getSOAPBody();
+        } catch (SOAPException e) {
+            throw new RuntimeException("create SOAPBody error");
+        }
+    }
+
+    public static MdSchemaFunctionsRestrictionsR discoverMdSchemaFunctionsRestrictions(SOAPElement restriction) {
+        Map<String, String> m = getMapValuesByTag(restriction, "RestrictionList");
+        return new MdSchemaFunctionsRestrictionsR(
+            Optional.ofNullable(OriginEnum.fromValue(m.get("ORIGIN"))),
+            Optional.ofNullable(InterfaceNameEnum.fromValue(m.get("INTERFACE_NAME"))),
+            Optional.ofNullable(m.get("LIBRARY_NAME"))
+        );
+    }
+
+    public static SOAPBody toMdSchemaFunctions(List<MdSchemaFunctionsResponseRow> rows) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    public static MdSchemaDimensionsRestrictionsR discoverMdSchemaDimensionsRestrictions(SOAPElement restrictionElement) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static SOAPBody toMdSchemaDimensions(List<MdSchemaDimensionsResponseRow> rows) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static MdSchemaCubesRestrictionsR discoverMdSchemaCubesRestrictions(SOAPElement restriction) {
+        Map<String, String> m = getMapValuesByTag(restriction, "RestrictionList");
+        return new MdSchemaCubesRestrictionsR(m.get("CATALOG_NAME"),
+            Optional.ofNullable(m.get("SCHEMA_NAME")),
+            Optional.ofNullable(m.get("CUBE_NAME")),
+            Optional.ofNullable(m.get("BASE_CUBE_NAME")),
+            Optional.ofNullable(CubeSourceEnum.fromValue(m.get("CUBE_SOURCE"))));
+    }
+
+    public static SOAPBody toMdSchemaCubes(List<MdSchemaCubesResponseRow> rows) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    private static Map<String, String> getMapValuesByTag(SOAPElement el, String tagName) {
+        NodeList nodeList = el.getElementsByTagName(tagName);
+        if (nodeList != null && nodeList.getLength() > 0) {
+            return getMapValues(nodeList.item(0).getChildNodes());
+        }
+        return Map.of();
+    }
+
+    private static Map<String, String> getMapValues(NodeList nl) {
+        Map<String, String> result = new HashMap<>();
+        for (int i = 0; i < nl.getLength(); i++) {
+            org.w3c.dom.Node n = nl.item(i);
+            result.put(n.getNodeName(), n.getTextContent());
+        }
+        return result;
+    }
+
+    private static SOAPElement addRoot(SOAPElement body) {
+        SOAPElement response = addChildElement(body, "DiscoverResponse");
+        SOAPElement ret = addChildElement(response, "return");
+        return addChildElement(ret, "root");
+    }
+
+    private static void addChildElement(SOAPElement element, String childElementName, String value) {
+        try {
+            if (value != null) {
+                element.addChildElement(childElementName).setTextContent(value);
+            }
+        } catch (SOAPException e) {
+            LOGGER.error("addChildElement {} error", childElementName);
+            throw new RuntimeException("addChildElement error", e);
+        }
+    }
+
+    private static SOAPElement addChildElement(SOAPElement element, String childElementName) {
+        try {
+            return element.addChildElement(childElementName);
+        } catch (SOAPException e) {
+            LOGGER.error("addChildElement {} error", childElementName);
+            throw new RuntimeException("addChildElement error", e);
+        }
     }
 
 }
