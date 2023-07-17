d01c5ac10858a7ab15f7b4103d152de6686a6fa8
dbulahov <bulahovdenis@gmail.com>
2023-07-17 13:55:03 +0300

    jakarta provider soap message Signed-off-by: dbulahov <bulahovdenis@gmail.com>

--- xmla/ws/jakarta.provider.soapmessage/src/main/java/org/eclipse/daanse/xmla/ws/jakarta/provider/soapmessage/XmlaApiAdapter.java
+++ xmla/ws/jakarta.provider.soapmessage/src/main/java/org/eclipse/daanse/xmla/ws/jakarta/provider/soapmessage/XmlaApiAdapter.java
@@ -21,9 +21,21 @@ import jakarta.xml.soap.SOAPMessage;
 import org.eclipse.daanse.xmla.api.XmlaService;
 import org.eclipse.daanse.xmla.api.discover.discover.properties.DiscoverPropertiesRequest;
 import org.eclipse.daanse.xmla.api.discover.discover.properties.DiscoverPropertiesResponseRow;
+import org.eclipse.daanse.xmla.api.discover.mdschema.cubes.MdSchemaCubesRequest;
+import org.eclipse.daanse.xmla.api.discover.mdschema.cubes.MdSchemaCubesResponseRow;
+import org.eclipse.daanse.xmla.api.discover.mdschema.demensions.MdSchemaDimensionsRequest;
+import org.eclipse.daanse.xmla.api.discover.mdschema.demensions.MdSchemaDimensionsResponseRow;
+import org.eclipse.daanse.xmla.api.discover.mdschema.functions.MdSchemaFunctionsRequest;
+import org.eclipse.daanse.xmla.api.discover.mdschema.functions.MdSchemaFunctionsResponseRow;
 import org.eclipse.daanse.xmla.model.record.discover.PropertiesR;
 import org.eclipse.daanse.xmla.model.record.discover.discover.properties.DiscoverPropertiesRequestR;
 import org.eclipse.daanse.xmla.model.record.discover.discover.properties.DiscoverPropertiesRestrictionsR;
+import org.eclipse.daanse.xmla.model.record.discover.mdschema.cubes.MdSchemaCubesRequestR;
+import org.eclipse.daanse.xmla.model.record.discover.mdschema.cubes.MdSchemaCubesRestrictionsR;
+import org.eclipse.daanse.xmla.model.record.discover.mdschema.demensions.MdSchemaDimensionsRequestR;
+import org.eclipse.daanse.xmla.model.record.discover.mdschema.demensions.MdSchemaDimensionsRestrictionsR;
+import org.eclipse.daanse.xmla.model.record.discover.mdschema.functions.MdSchemaFunctionsRequestR;
+import org.eclipse.daanse.xmla.model.record.discover.mdschema.functions.MdSchemaFunctionsRestrictionsR;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,7 +51,34 @@ public class XmlaApiAdapter {
         this.xmlaService = xmlaService;
     }
 
+    private static final String MDSCHEMA_FUNCTIONS = "MDSCHEMA_FUNCTIONS";
+    private static final String MDSCHEMA_DIMENSIONS = "MDSCHEMA_DIMENSIONS";
+    private static final String MDSCHEMA_CUBES = "MDSCHEMA_CUBES";
+    private static final String MDSCHEMA_ACTIONS = "MDSCHEMA_ACTIONS";
+    private static final String DBSCHEMA_TABLES = "DBSCHEMA_TABLES";
+    private static final String DISCOVER_LITERALS = "DISCOVER_LITERALS";
+    private static final String DISCOVER_KEYWORDS = "DISCOVER_KEYWORDS";
+    private static final String DISCOVER_ENUMERATORS = "DISCOVER_ENUMERATORS";
+    private static final String DISCOVER_SCHEMA_ROWSETS = "DISCOVER_SCHEMA_ROWSETS";
     private static final String DISCOVER_PROPERTIES = "DISCOVER_PROPERTIES";
+    private static final String DBSCHEMA_CATALOGS = "DBSCHEMA_CATALOGS";
+    private static final String DISCOVER_DATASOURCES = "DISCOVER_DATASOURCES";
+    private static final String DISCOVER_XML_METADATA = "DISCOVER_XML_METADATA";
+    private static final String DBSCHEMA_COLUMNS = "DBSCHEMA_COLUMNS";
+    private static final String DBSCHEMA_PROVIDER_TYPES = "DBSCHEMA_PROVIDER_TYPES";
+    private static final String DBSCHEMA_SCHEMATA = "DBSCHEMA_SCHEMATA";
+    private static final String DBSCHEMA_SOURCE_TABLES = "DBSCHEMA_SOURCE_TABLES";
+    private static final String DBSCHEMA_TABLES_INFO = "DBSCHEMA_TABLES_INFO";
+    private static final String MDSCHEMA_HIERARCHIES = "MDSCHEMA_HIERARCHIES";
+    private static final String MDSCHEMA_LEVELS = "MDSCHEMA_LEVELS";
+    private static final String MDSCHEMA_MEASUREGROUP_DIMENSIONS = "MDSCHEMA_MEASUREGROUP_DIMENSIONS";
+    private static final String MDSCHEMA_MEASURES = "MDSCHEMA_MEASURES";
+    private static final String MDSCHEMA_MEMBERS = "MDSCHEMA_MEMBERS";
+    private static final String MDSCHEMA_PROPERTIES = "MDSCHEMA_PROPERTIES";
+    private static final String MDSCHEMA_SETS = "MDSCHEMA_SETS";
+    private static final String MDSCHEMA_KPIS = "MDSCHEMA_KPIS";
+    private static final String MDSCHEMA_MEASUREGROUPS = "MDSCHEMA_MEASUREGROUPS";
+
 
     public SOAPMessage handleRequest(SOAPMessage message) {
         try {
@@ -129,11 +168,32 @@ public class XmlaApiAdapter {
         SOAPBody discoverResponse = null;
 
         switch (requestType) {
-        case DISCOVER_PROPERTIES -> discoverResponse = handleDiscoverProperties(properties, restrictionElement);
+            case MDSCHEMA_FUNCTIONS -> discoverResponse = handleMdSchemaFunctions(properties, restrictionElement);
+            case MDSCHEMA_DIMENSIONS -> discoverResponse = handleMdSchemaDimensions(properties, restrictionElement);
+            case MDSCHEMA_CUBES -> discoverResponse = handleMdSchemaCubes(properties, restrictionElement);
+            case DISCOVER_PROPERTIES -> discoverResponse = handleDiscoverProperties(properties, restrictionElement);
         default -> throw new IllegalArgumentException("Unexpected value: " + requestType);
 
         }
         return discoverResponse;
+    }
+
+    private SOAPBody handleMdSchemaCubes(PropertiesR propertiesR, SOAPElement restrictionElement) {
+        MdSchemaCubesRestrictionsR restrictionsR = Convert.discoverMdSchemaCubesRestrictions(restrictionElement);
+        MdSchemaCubesRequest request = new MdSchemaCubesRequestR(propertiesR, restrictionsR);
+        List<MdSchemaCubesResponseRow> rows = xmlaService.discover()
+            .mdSchemaCubes(request);
+
+        return Convert.toMdSchemaCubes(rows);
+    }
+
+    private SOAPBody handleMdSchemaDimensions(PropertiesR propertiesR, SOAPElement restrictionElement) {
+        MdSchemaDimensionsRestrictionsR restrictionsR = Convert.discoverMdSchemaDimensionsRestrictions(restrictionElement);
+        MdSchemaDimensionsRequest request = new MdSchemaDimensionsRequestR(propertiesR, restrictionsR);
+        List<MdSchemaDimensionsResponseRow> rows = xmlaService.discover()
+            .mdSchemaDimensions(request);
+
+        return Convert.toMdSchemaDimensions(rows);
     }
 
     private SOAPBody handleDiscoverProperties(PropertiesR propertiesR, SOAPElement restrictionElement) {
@@ -144,6 +204,16 @@ public class XmlaApiAdapter {
                 .discoverProperties(request);
 
         return Convert.toDiscoverProperties(rows);
+    }
+
+    private SOAPBody handleMdSchemaFunctions(PropertiesR propertiesR, SOAPElement restrictionElement) {
+
+        MdSchemaFunctionsRestrictionsR restrictionsR = Convert.discoverMdSchemaFunctionsRestrictions(restrictionElement);
+        MdSchemaFunctionsRequest request = new MdSchemaFunctionsRequestR(propertiesR, restrictionsR);
+        List<MdSchemaFunctionsResponseRow> rows = xmlaService.discover()
+            .mdSchemaFunctions(request);
+
+        return Convert.toMdSchemaFunctions(rows);
     }
 
 }
