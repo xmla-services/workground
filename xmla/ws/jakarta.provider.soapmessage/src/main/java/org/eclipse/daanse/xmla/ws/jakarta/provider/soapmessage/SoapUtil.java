5bf88c867fdaa926298a5f7695f239b89b781783
dbulahov <bulahovdenis@gmail.com>
2023-07-24 18:43:14 +0300

    jakarta provider soap message Signed-off-by: dbulahov <bulahovdenis@gmail.com>

---
+++ xmla/ws/jakarta.provider.soapmessage/src/main/java/org/eclipse/daanse/xmla/ws/jakarta/provider/soapmessage/SoapUtil.java
@@ -0,0 +1,422 @@
+/*
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   SmartCity Jena - initial
+ *   Stefan Bischof (bipolis.org) - initial
+ */
+package org.eclipse.daanse.xmla.ws.jakarta.provider.soapmessage;
+
+import jakarta.xml.soap.MessageFactory;
+import jakarta.xml.soap.SOAPBody;
+import jakarta.xml.soap.SOAPElement;
+import jakarta.xml.soap.SOAPException;
+import jakarta.xml.soap.SOAPMessage;
+import org.eclipse.daanse.xmla.api.engine200.WarningLocationObject;
+import org.eclipse.daanse.xmla.api.exception.ErrorType;
+import org.eclipse.daanse.xmla.api.exception.Exception;
+import org.eclipse.daanse.xmla.api.exception.MessageLocation;
+import org.eclipse.daanse.xmla.api.exception.Messages;
+import org.eclipse.daanse.xmla.api.exception.StartEnd;
+import org.eclipse.daanse.xmla.api.exception.WarningType;
+import org.eclipse.daanse.xmla.api.execute.statement.StatementResponse;
+import org.eclipse.daanse.xmla.api.mddataset.Axes;
+import org.eclipse.daanse.xmla.api.mddataset.AxesInfo;
+import org.eclipse.daanse.xmla.api.mddataset.Axis;
+import org.eclipse.daanse.xmla.api.mddataset.AxisInfo;
+import org.eclipse.daanse.xmla.api.mddataset.CellData;
+import org.eclipse.daanse.xmla.api.mddataset.CellInfo;
+import org.eclipse.daanse.xmla.api.mddataset.CellInfoItem;
+import org.eclipse.daanse.xmla.api.mddataset.CellSetType;
+import org.eclipse.daanse.xmla.api.mddataset.CellType;
+import org.eclipse.daanse.xmla.api.mddataset.CubeInfo;
+import org.eclipse.daanse.xmla.api.mddataset.HierarchyInfo;
+import org.eclipse.daanse.xmla.api.mddataset.Mddataset;
+import org.eclipse.daanse.xmla.api.mddataset.MemberType;
+import org.eclipse.daanse.xmla.api.mddataset.MembersType;
+import org.eclipse.daanse.xmla.api.mddataset.NormTupleSet;
+import org.eclipse.daanse.xmla.api.mddataset.OlapInfo;
+import org.eclipse.daanse.xmla.api.mddataset.SetListType;
+import org.eclipse.daanse.xmla.api.mddataset.TupleType;
+import org.eclipse.daanse.xmla.api.mddataset.TuplesType;
+import org.eclipse.daanse.xmla.api.mddataset.Type;
+import org.eclipse.daanse.xmla.api.mddataset.Union;
+import org.eclipse.daanse.xmla.api.msxmla.MembersLookup;
+import org.eclipse.daanse.xmla.api.msxmla.NormTuplesType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class SoapUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SoapUtil.class);
+
+    public static SOAPBody toStatementResponse(StatementResponse statementResponse) {
+        SOAPBody body = createSOAPBody();
+        SOAPElement root = addRoot(body);
+        addMdDataSet(root, statementResponse.mdDataSet());
+        //TODO
+        return body;
+    }
+
+    private static void addMdDataSet(SOAPElement e, Mddataset it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "mddataset");
+            addOlapInfo(el, it.olapInfo());
+            addAxes(el, it.axes());
+            addCellData(el, it.cellData());
+            addException(el, it.exception());
+            addMessages(el, it.messages());
+        }
+    }
+
+    private static void addMessages(SOAPElement e, Messages it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "Messages");
+            addExceptionTypeList(el, it.warningOrError());
+
+        }
+    }
+
+    private static void addExceptionTypeList(
+        SOAPElement e,
+        List<org.eclipse.daanse.xmla.api.exception.Type> list) {
+        if (list != null) {
+            list.forEach(it -> addExceptionType(e, it));
+        }
+    }
+
+    private static void addExceptionType(SOAPElement e, org.eclipse.daanse.xmla.api.exception.Type it) {
+        if (it != null) {
+            if (it instanceof WarningType warningType) {
+                addWarningType(e, warningType);
+            }
+            if (it instanceof ErrorType errorType) {
+                addErrorType(e, errorType);
+            }
+        }
+    }
+
+    private static void addErrorType(SOAPElement e, ErrorType it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "Error");
+
+            addChildElement(el, "Callstack", it.callstack());
+            addChildElement(el, "ErrorCode", String.valueOf(it.errorCode()));
+            addMessageLocation(el, it.location());
+            addChildElement(el, "Description", it.description());
+            addChildElement(el, "Source", it.source());
+            addChildElement(el, "HelpFile", it.helpFile());
+        }
+    }
+
+    private static void addWarningType(SOAPElement e, WarningType it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "Warning");
+            addChildElement(el, "WarningCode", String.valueOf(it.warningCode()));
+            addMessageLocation(el, it.location());
+            addChildElement(el, "Description", it.description());
+            addChildElement(el, "Source", it.source());
+            addChildElement(el, "HelpFile", it.helpFile());
+        }
+    }
+
+    private static void addMessageLocation(SOAPElement e, MessageLocation it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "Location");
+            addStartEnd(el, "Start", it.start());
+            addStartEnd(el, "End", it.end());
+            addChildElement(el, "LineOffset", String.valueOf(it.lineOffset()));
+            addChildElement(el, "TextLength", String.valueOf(it.textLength()));
+            addWarningLocationObject(el, "SourceObject", it.sourceObject());
+            addWarningLocationObject(el, "DependsOnObject", it.dependsOnObject());
+            addChildElement(el, "RowNumber", String.valueOf(it.rowNumber()));
+        }
+
+    }
+
+    private static void addWarningLocationObject(SOAPElement el, String tagName, WarningLocationObject sourceObject) {
+        //SourceObject
+    }
+
+    private static void addStartEnd(SOAPElement el, String tagName, StartEnd end) {
+        //End
+    }
+
+
+    private static void addException(SOAPElement e, Exception it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "Exception");
+        }
+    }
+
+    private static void addCellData(SOAPElement e, CellData it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "CellData");
+            addCellTypeList(el, it.cell());
+            addCellSetType(el, it.cellSet());
+        }
+    }
+
+    private static void addCellSetType(SOAPElement e, CellSetType it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "CellSet");
+            //TODO
+        }
+    }
+
+    private static void addCellTypeList(SOAPElement el, List<CellType> list) {
+        if (list != null) {
+            list.forEach(it -> addCellType(el, it));
+        }
+    }
+
+    private static void addCellType(SOAPElement e, CellType it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "Cell");
+            //TODO
+        }
+    }
+
+    private static void addAxes(SOAPElement e, Axes it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "Axes");
+            addAxisList(el, it.axis());
+        }
+    }
+
+    private static void addAxisList(SOAPElement e, List<Axis> list) {
+        if (list != null) {
+            list.forEach(it -> addAxis(e, it));
+        }
+    }
+
+    private static void addAxis(SOAPElement e, Axis it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "Axis");
+            addTypeList(el, it.setType());
+            addChildElement(el, "name", it.name());
+        }
+    }
+
+    private static void addTypeList(SOAPElement e, List<Type> list) {
+        if (list != null) {
+            list.forEach(it -> addType(e, it));
+        }
+    }
+
+    private static void addType(SOAPElement e, Type it) {
+        if (it != null) {
+            if (it instanceof MembersType membersType) {
+                addMembersType(e, membersType);
+            }
+            if (it instanceof TuplesType tuplesType) {
+                addTuplesType(e, tuplesType);
+            }
+            if (it instanceof SetListType setListType) {
+                addSetListType(e, setListType);
+            }
+            if (it instanceof NormTupleSet normTupleSet) {
+                addNormTupleSet(e, normTupleSet);
+            }
+            if (it instanceof Union union) {
+                addUnion(e, union);
+            }
+        }
+    }
+
+    private static void addUnion(SOAPElement e, Union it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "Union");
+            addTypeList(el, it.setType());
+        }
+    }
+
+    private static void addNormTupleSet(SOAPElement e, NormTupleSet it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "NormTupleSet");
+            addNormTuplesType(el, it.normTuples());
+            addTupleTypeList(el, it.membersLookup());
+        }
+    }
+
+    private static void addTupleTypeList(SOAPElement e, MembersLookup list) {
+        if (list != null) {
+            SOAPElement el = addChildElement(e, "MembersLookup");
+            if (list.members() != null) {
+                list.members().forEach(it -> addTupleType(el, "Members", it));
+            }
+        }
+    }
+
+    private static void addNormTuplesType(SOAPElement e, NormTuplesType it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "NormTuples");
+            //TODO
+
+        }
+    }
+
+    private static void addSetListType(SOAPElement e, SetListType it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "CrossProduct");
+            addTypeList(el, it.setType());
+            addChildElement(el, "Size", String.valueOf(it.size()));
+        }
+    }
+
+    private static void addTuplesType(SOAPElement e, TuplesType it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "Tuples");
+            addTuplesTypeList(el, it.tuple());
+        }
+
+    }
+
+    private static void addTuplesTypeList(SOAPElement e, List<TupleType> list) {
+        if (list != null) {
+            list.forEach(it -> addTupleType(e, "Tuple", it));
+        }
+    }
+
+    private static void addTupleType(SOAPElement e, String tagName, TupleType it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, tagName);
+            addMemberTypeList(el, it.member());
+        }
+
+    }
+
+    private static void addMemberTypeList(SOAPElement e, List<MemberType> list) {
+        if (list != null) {
+            list.forEach(it -> addMemberType(e, it));
+        }
+    }
+
+    private static void addMemberType(SOAPElement e, MemberType it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "Member");
+            addCellInfoItemList(el, it.any());
+            addChildElement(el, "Hierarchy", it.hierarchy());
+        }
+    }
+
+    private static void addMembersType(SOAPElement e, MembersType it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "Members");
+            addMemberTypeList(el, it.member());
+            addChildElement(el, "Hierarchy", it.hierarchy());
+        }
+    }
+
+    private static void addOlapInfo(SOAPElement e, OlapInfo it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "OlapInfo");
+            addCubeInfo(el, it.cubeInfo());
+            addAxesInfo(el, it.axesInfo());
+            addCellInfo(el, it.cellInfo());
+        }
+    }
+
+    private static void addCellInfo(SOAPElement e, CellInfo it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "CellInfo");
+            addCellInfoItemList(el, it.any());
+        }
+    }
+
+    private static void addCellInfoItemList(SOAPElement e, List<CellInfoItem> list) {
+        if (list != null) {
+            list.forEach(it -> addCellInfoItem(e, it));
+        }
+    }
+
+    private static void addCellInfoItem(SOAPElement e, CellInfoItem it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, it.tagName());
+            el.setAttribute("name", it.name());
+            it.type().ifPresent(v -> el.setAttribute("type", v));
+        }
+    }
+
+    private static void addAxesInfo(SOAPElement e, AxesInfo it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "AxesInfo");
+            addAxisInfoList(el, it.axisInfo());
+        }
+    }
+
+    private static void addAxisInfoList(SOAPElement el, List<AxisInfo> list) {
+        if (list != null) {
+            list.forEach(it -> addAxisInfo(el, it));
+        }
+    }
+
+    private static void addAxisInfo(SOAPElement e, AxisInfo it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "AxisInfo");
+            addChildElement(el, "name", it.name());
+            addHierarchyInfoList(el, it.hierarchyInfo());
+        }
+    }
+
+    private static void addHierarchyInfoList(SOAPElement el, List<HierarchyInfo> list) {
+        if (list != null) {
+            list.forEach(it -> addHierarchyInfo(el, it));
+        }
+    }
+
+    private static void addHierarchyInfo(SOAPElement e, HierarchyInfo it) {
+        if (it != null) {
+            SOAPElement el = addChildElement(e, "HierarchyInfo");
+            addCellInfoItemList(el, it.any());
+            addChildElement(el, "name", it.name());
+        }
+    }
+
+    private static void addCubeInfo(SOAPElement el, CubeInfo it) {
+        //TODO
+    }
+
+    private static SOAPBody createSOAPBody() {
+        try {
+            SOAPMessage message = MessageFactory.newInstance().createMessage();
+            return message.getSOAPBody();
+        } catch (SOAPException e) {
+            throw new RuntimeException("create SOAPBody error");
+        }
+    }
+
+    private static SOAPElement addRoot(SOAPElement body) {
+        SOAPElement response = addChildElement(body, "DiscoverResponse");
+        SOAPElement ret = addChildElement(response, "return");
+        return addChildElement(ret, "root");
+    }
+
+    private static void addChildElement(SOAPElement element, String childElementName, String value) {
+        try {
+            if (value != null) {
+                element.addChildElement(childElementName).setTextContent(value);
+            }
+        } catch (SOAPException e) {
+            LOGGER.error("addChildElement {} error", childElementName);
+            throw new RuntimeException("addChildElement error", e);
+        }
+    }
+
+    private static SOAPElement addChildElement(SOAPElement element, String childElementName) {
+        try {
+            return element.addChildElement(childElementName);
+        } catch (SOAPException e) {
+            LOGGER.error("addChildElement {} error", childElementName);
+            throw new RuntimeException("addChildElement error", e);
+        }
+    }
+
+}
