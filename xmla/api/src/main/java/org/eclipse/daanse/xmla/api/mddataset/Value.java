d1fd69adf6cf36d65f27f83e0aed37b7731d78b6
dbulahov <bulahovdenis@gmail.com>
2023-08-09 15:50:10 +0300

    add value to cell Signed-off-by: dbulahov <bulahovdenis@gmail.com>

--- xmla/api/src/main/java/org/eclipse/daanse/xmla/api/mddataset/Value.java
+++ xmla/api/src/main/java/org/eclipse/daanse/xmla/api/mddataset/Value.java
@@ -1,9 +1,14 @@
 package org.eclipse.daanse.xmla.api.mddataset;
 
+import org.eclipse.daanse.xmla.api.common.enums.CellTypeEnum;
+
 import java.util.List;
 
 public interface Value {
 
     List<CellTypeError> error();
 
+    String value();
+
+    CellTypeEnum type();
 }
