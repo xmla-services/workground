643cf68f9f5e376d77263f6a3598802897213125
Stefan Bischof <stbischof@bipolis.org>
2023-09-19 18:45:03 +0200

    remove generates code - version
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ mondrian/src/main/java/mondrian/olap4j/MondrianOlap4jDriverVersion.java
@@ -0,0 +1,16 @@
+/*
+ * Project version information. Generated - do not modify.
+ */
+package mondrian.olap4j;
+/**
+ * Version information for the Mondrian olap4j driver. (Generated.)
+ */
+//WAS AUTO GENERATED. JUST COPIED . TO BE REMOVED
+class MondrianOlap4jDriverVersion {
+    static final String NAME = "Mondrian olap4j driver";
+    static final String VERSION = "0.0.1-SNAPSHOT";
+    static final int MAJOR_VERSION = 8;
+    static final int MINOR_VERSION = 000;
+}
+
+// End MondrianOlap4jDriverVersion.java
