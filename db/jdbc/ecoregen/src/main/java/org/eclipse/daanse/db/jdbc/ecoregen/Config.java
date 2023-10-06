e0f976d1c501b8b643ce13f6523ed04ce1a949cb
Stefan <stbischof@bipolis.org>
2023-10-06 17:16:22 +0200

    ecore generator
    Signed-off-by: Stefan <stbischof@bipolis.org>

---
+++ db/jdbc/ecoregen/src/main/java/org/eclipse/daanse/db/jdbc/ecoregen/Config.java
@@ -0,0 +1,22 @@
+package org.eclipse.daanse.db.jdbc.ecoregen;
+
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+@ObjectClassDefinition()
+public interface Config {
+//
+//	@AttributeDefinition
+//	default String packageName() {
+//		return "default";
+//	}
+//
+//	@AttributeDefinition
+//	default String nsUri() {
+//		return "http://model.data.jena.de/database.xyz";
+//	}
+//
+//	@AttributeDefinition
+//	default String nsPrefix() {
+//		return "default";
+//	}
+}
