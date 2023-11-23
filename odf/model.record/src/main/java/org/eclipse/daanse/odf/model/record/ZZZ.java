774c6b43836cc7c9386fb8f7285f7dccc809a8b4
Stefan <stbischof@bipolis.org>
2023-11-23 19:16:41 +0100

    rm unused odf record
    Signed-off-by: Stefan <stbischof@bipolis.org>

--- odf/model.record/src/main/java/org/eclipse/daanse/odf/model/record/ZZZ.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.daanse.odf.model.record;
-
-import io.soabase.recordbuilder.core.RecordInterface;
-
-@RecordInterface.Include(classes = { org.eclipse.daanse.odf.api.SpreadSheet.class },addRecordBuilder = true)
-public class ZZZ {
-
-}
